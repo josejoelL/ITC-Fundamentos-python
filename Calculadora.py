0f11b8a2d4b49199e5b65fecb7c00159555242e0
JoJo <18170365@itculiacan.edu.mx>
2024-05-23 22:09:04 -0700

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+valor1 = 1
+valor2 = 2
+
+sumaValores = valor1 + valor2
+restaValores = valor1 - valor2
+divicionValores = valor1 / valor2
+moduloValores = valor1 % valor2
+
+print("La suma es : "+ str(sumaValores) )
+print("La suma es : "+ str(restaValores) )
+print("La suma es : "+ str(divicionValores) )
+print("La suma es : "+ str(moduloValores) )
